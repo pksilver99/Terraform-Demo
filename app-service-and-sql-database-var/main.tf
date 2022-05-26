f1e40f9e209b1bcef402c3948ebfffbb795a57cc
pksilver99 <91254614+pksilver99@users.noreply.github.com>
2022-05-26 14:45:27 +0100

    Update main.tf

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -1,3 +1,8 @@
+
+# provides config details for Azure TerraForm provider
+provider "azurerm" {
+  features {}
+
 resource "azurerm_resource_group" "RG-Terraform" {
   name     = "terraform-resource-group"
   location = "West Europe"
